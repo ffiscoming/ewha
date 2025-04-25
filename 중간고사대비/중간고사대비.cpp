26c88128bf3e0320dc2ac507b6715ca527d13978
이세민 <semin2837@gmail.com>
2025-04-25 20:54:26 +0900

    대비마마

---
+++ 중간고사대비/중간고사대비.cpp
@@ -0,0 +1,140 @@
+﻿#include <iostream>
+using namespace std;
+
+struct TermDeposit
+{
+	char name[20];
+	int deposit;
+	double interestRate;
+	bool interestType;
+	int year;
+};
+struct Account
+{
+	char ID[20];
+	int number;
+	TermDeposit* termDeposits;
+	int *balances;
+	int* interestTaxes;
+};
+
+void setAccount(Account&);
+void showAccount(const Account&);
+char* showComma(const int);
+
+int main()
+{
+	Account account = {};
+	setAccount(account);
+	showAccount(account);
+	return 0;
+}
+
+void setTermDeposit(TermDeposit& termDeposit)
+{
+	cout << "Enter name: ";
+	cin >> termDeposit.name;
+	cout << "Enter deposit: ";
+	cin >> termDeposit.deposit;
+	cout << "Enter interest rate: ";
+	cin >> termDeposit.interestRate;
+	cout << "Enter interest type (0 for simple, 1 for compound): ";
+	cin >> termDeposit.interestType;
+	cout << "Enter year: ";
+	cin >> termDeposit.year;
+}
+void showTermDeposit(const TermDeposit& termDeposit)
+{
+	cout << "Name: " << termDeposit.name << endl;
+	cout << "Deposit: " << showComma(termDeposit.deposit) << endl;
+	cout << "Interest Rate: " << termDeposit.interestRate << "%" << endl;
+	cout << "Interest Type: ";
+	if (termDeposit.interestType)
+		cout << "Simple" << endl;
+	else
+		cout << "Compound" << endl;
+	cout << "Year: " << termDeposit.year << endl;
+}
+int claSimpleInterest(const TermDeposit& termDeposit)
+{
+	return int(termDeposit.deposit * termDeposit.interestRate / 100 * termDeposit.year);
+}
+int claCompoundInterest(const TermDeposit& termDeposit)
+{
+	return int(termDeposit.deposit * (pow(1 + termDeposit.interestRate / 100, termDeposit.year) - 1));
+}
+int calInterest(const TermDeposit& termDeposit)
+{
+	if (termDeposit.interestType)
+		return claSimpleInterest(termDeposit);
+	else
+		return claCompoundInterest(termDeposit);
+}
+int calInterestTax(const TermDeposit& termDeposit)
+{
+	return int(calInterest(termDeposit) * 15.4/100);
+}
+int calBalance(const TermDeposit& termDeposit)
+{
+	return termDeposit.deposit + calInterest(termDeposit) - calInterestTax(termDeposit);
+}
+void setAccount(Account& theAccount)
+{
+	cout << "Enter ID: ";
+	cin >> theAccount.ID;
+	cout << "Enter number of term deposits: ";
+	cin >> theAccount.number;
+
+	theAccount.termDeposits = new TermDeposit[theAccount.number];
+	theAccount.balances = new int[theAccount.number];
+	theAccount.interestTaxes = new int[theAccount.number];
+	for (int i = 0; i < theAccount.number; ++i)
+	{
+		cout << "-------------------------\n";
+		cout << "account " << i + 1 << ":" << endl;
+		setTermDeposit(theAccount.termDeposits[i]);
+		theAccount.balances[i] = calBalance(theAccount.termDeposits[i]);
+		theAccount.interestTaxes[i] = calInterestTax(theAccount.termDeposits[i]);
+	}
+}
+void showAccount(const Account& theAccount)
+{
+	cout << "==========================\n";
+	cout << theAccount.ID << "'s account info\n";
+	for (int i = 0; i < theAccount.number; i++)
+	{
+		cout << "==========================\n";
+		showTermDeposit(theAccount.termDeposits[i]);
+		cout << "Balance: " << showComma(theAccount.balances[i]) << endl;
+		cout << "Interest Tax: " << showComma(theAccount.interestTaxes[i]) << endl;
+	}
+	cout << "==========================\n";
+}
+char* showComma(const int n)
+{
+	int len(0), m(n);
+	while (m > 0)
+	{
+		m /= 10;
+		len++;
+	}
+	int num_comma(0);
+	for (int i = 1; i <= len; i++)
+		if (!(i % 3)) num_comma++;
+	char* char_comma = new char[len + num_comma + 1];
+	char_comma[len + num_comma] = '\0';
+	m = n;
+	int index_char(len + num_comma - 1);
+	for (int i = 1; i <= len; i++)
+	{
+		char_comma[index_char] = char('0' + m % 10);
+		index_char--;
+		m /= 10;
+		if (!(i % 3))
+		{
+			char_comma[index_char] = '.';
+			index_char--;
+		}
+	}
+	return char_comma;
+}
---
+++ 중간고사대비를대비/중간고사대비를대비.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int classifyRate(int income) 
+{
+    if (income <= 1200) return 1;
+    else if (income <= 4600) return 2;
+    else if (income <= 8800) return 3;
+    else if (income <= 15000) return 4;
+    else if (income <= 30000) return 5;
+    else if (income <= 50000) return 6;
+    else return 7;
+}
+
+int taxRate(int rateClass) 
+{
+    switch (rateClass) 
+    {
+    case 1: return 6;
+    case 2: return 15;
+    case 3: return 24;
+    case 4: return 35;
+    case 5: return 38;
+    case 6: return 40;
+    case 7: return 42;
+    default: exit(1);
+    }
+}
+
+void calTax(int income, int& tax) 
+{
+    int rateClass = classifyRate(income);
+    int rate = taxRate(rateClass);
+    tax = income * rate / 100;
+}
+
+void showResults(int income, int tax) 
+{
+    cout << income << ", tax: " << tax << endl;
+}
+
+void showResults(int i, int income, int tax) 
+{
+    cout << i << ": " << income << ", tax: " << tax << endl;
+}
+
+int main() 
+{
+    srand(time(0));
+    int n;
+    cout << "Enter the iteration number: ";
+    cin >> n;
+
+    int totalTax = 0;
+    for (int i = 1; i <= n; ++i) 
+    {
+        int income = rand() % 70001 + 1000;
+        int tax;
+        calTax(income, tax);
+        showResults(i, income, tax);
+        totalTax += tax;
+    }
+
+    cout << "tax average = " << totalTax / n << endl;
+
+    return 0;
+}
